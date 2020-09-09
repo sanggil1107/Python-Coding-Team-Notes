55851c4c0b3f0d08ecdf848ce15f1e76ab3d9735
sanggil1107 <yangpang1107@gmail.com>
2020-09-09 23:58:34 +0900

    team note

---
+++ Searching/BFS.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+def bfs(x):
+  queue = deque([x])
+  visited[x] = True
+
+  while queue:
+    v = queue.popleft()
+    print(v, end = ' ')
+    for i in graph[v]:
+      if not visited[i]:
+        queue.append(i)
+        visited[i] = True
+
+n, m, start = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+  x, y = map(int, input().split())
+  graph[x].append(y)
+  graph[y].append(x)
+
+for e in graph:
+  e.sort()
+
+visited = [False] * (n+1)
+dfs(start)
---
+++ Searching/DFS.py
@@ -0,0 +1,21 @@
+
+def dfs(x):
+  print(x, end = ' ')
+  visited[x] = True
+  for i in graph[x]:
+    if not visited[i]:
+      dfs(i)
+
+
+n, m, start = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+  x, y = map(int, input().split())
+  graph[x].append(y)
+  graph[y].append(x)
+
+for e in graph:
+  e.sort()
+
+visited = [False] * (n+1)
+dfs(start)
